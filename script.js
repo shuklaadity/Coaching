ef846d4551e762ae7ab3d6e0be57a195ed4daf49
shuklaadity <aditya02shukla1@gmail.com>
2023-09-15 13:50:07 +0530

    add new style

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 var  service=document.querySelector("#sirviceclick")
 var servicediv=document.querySelector(".serviclist")
 
+var email=document.getElementById("email");
+var pas=document.getElementById("password");
 
+function showfun(){
+  alert("login success full");
+}
  
-  service.onclick =  function(){
-        alert("aditya shukla")
-    }
+ 
